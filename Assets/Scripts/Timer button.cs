993b74b3ec3dbe27ba3bdac149859d84bd031ce7
Atym-git <chuvachelaad@gmail.com>
2024-09-22 23:59:52 +0300

    Initial commit

---
+++ Assets/Scripts/Timer button.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.UIElements;
+
+public class Timerbutton : MonoBehaviour
+{
+    [SerializeField] private int time;
+    [SerializeField] private Image image;
+    [SerializeField] private float image_fillspeed;
+    [SerializeField] private Button button;
+
+    private void Start()
+    {
+        button.onClick.AddListener(() => StartCoroutine(Timer()));
+
+    }
+
+    IEnumerator Timer()
+    {
+           for (int i = 0; i <= time; i++)
+        {
+            Image_fill();
+            yield return new WaitForSeconds(1);
+        }
+    }    
+
+
+    void OnclickButton()
+    {
+        
+
+        
+    }
+
+    private void Image_fill()
+    {
+        if (image.fillAmount > 0)
+            { image.fillAmount = image.fillAmount - image_fillspeed;
+
+            }
+        else
+        {
+            image.fillAmount = 1;
+        }
+    }    
+
+}
